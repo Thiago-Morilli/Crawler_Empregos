84b80f351c72ff3362acf466017b0ae476ee4332
Thiago-Morilli <thiagomorilli50@gmail.com>
2025-04-14 18:48:06 +0100

    updated

---
+++ Empregos/bot_telegram/bot.py
@@ -0,0 +1,38 @@
+import telebot 
+from DataBase.Mysql_connection import Mysql_Connector
+
+CHAVE_API = "7594338375:AAEfZMrMETc7_8Kehdh4gvBlWFmkDFE4VNY"
+
+bot = telebot.TeleBot(CHAVE_API)
+
+
+@bot.message_handler(func=lambda msg: True)  
+def responder_empregos_por_cidade(mensagem):
+    city = mensagem.text.strip()  
+
+    connector = Mysql_Connector.Connection()
+    cursor = connector[0]
+    db_connection = connector[1]
+       
+    query = "SELECT title, company_name, type_work, salary FROM Empregos WHERE location = %s"
+    cursor.execute(query, (city,))
+    jobs = cursor.fetchall()
+
+    if jobs:
+        resposta = f"Todos empregos em {city}:\n\n"
+        for job in jobs:
+            title, company, work_type, salary = job
+            resposta += (
+                f"Cargo: {title}\n"
+                f"Empresa: {company}\n"
+                f"Modelo: {work_type}\n"
+                f"Salário: {salary}\n"
+                f"-----------------------------\n"
+            )
+
+    bot.reply_to(mensagem, resposta, parse_mode="Markdown")
+
+    cursor.close()
+    db_connection.close()
+
+bot.polling()
